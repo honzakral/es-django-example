013bfadecbcfd8d52e4f27c7ed3f8fd7d88ad70d
Honza Král <honza.kral@gmail.com>
2015-09-05 14:25:38 +0200

    Use parnet/child in search and add tag filters

--- qa/views.py
+++ qa/views.py
@@ -2,6 +2,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.shortcuts import render
 
+from elasticsearch_dsl import Q
+
 from .models import Question
 from .search import Question as QuestionDoc
 
@@ -18,6 +20,14 @@ def search(request):
     s = QuestionDoc.search()
     if 'q' in request.GET:
         query = request.GET['q']
-        s = s.query('multi_match', fields=['tags^10', 'title', 'body'], query=query)
+        # query in tags, title and body for query
+        q = Q('multi_match', fields=['tags^10', 'title', 'body'], query=query)
+        # also find questions that have answers matching query
+        q |= Q('has_child', type='answer', query=Q('match', body=query))
+        s = s.query(q)
+
+    # add tags filters
+    if 'tags' in request.GET:
+        s = s.filter('terms', tags=request.GET.getlist('tags'))
 
     return render(request, 'qa/question_list.html', {'object_list': s.execute()})
