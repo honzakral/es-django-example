89733c5d9506715270e8797071d6a9a74eb8382f
Honza Král <honza.kral@gmail.com>
2015-09-05 14:16:36 +0200

    Simple search view

--- qa/views.py
+++ qa/views.py
@@ -1,5 +1,9 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.conf import settings
+from django.shortcuts import render
+
+from elasticsearch_dsl import Search
 
 from .models import Question
 
@@ -12,3 +16,15 @@ class QuestionList(ListView):
 class QuestionDetail(DetailView):
     model = Question
 
+def search(request):
+    s = Search(index=settings.ES_INDEX, doc_type='question')
+    if 'q' in request.GET:
+        s = s.query(
+            'multi_match',
+            fields=['title', 'tags', 'body'],
+            query=request.GET['q']
+        )
+
+    r = s.execute()
+
+    return render(request, 'qa/question_list.html', {'object_list': r.hits})
