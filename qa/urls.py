074b9b29074b6302ae4a296de2ae8406532f570f
varunpurohit76 <varunpurohit76@gmail.com>
2017-02-19 14:09:58 +0530

    to django 1.10. also changed schema for Null UserID

--- qa/urls.py
+++ qa/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import QuestionDetail, QuestionList, search
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', QuestionList.as_view(), name='qa-list'),
     url(r'^(?P<pk>\d+)/$', QuestionDetail.as_view(), name='qa-question'),
     url(r'^search/$', search, name='qa-search'),
-)
+]
